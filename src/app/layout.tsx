0314ca2b6adbc31db61893004b3adee9774bf0c6
brr naturals <madhu12123reddy@gmail.com>
2026-04-25 02:04:22 +0530

    new change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import type { Metadata, Viewport } from 'next';
 import { Plus_Jakarta_Sans, Fraunces } from 'next/font/google';
-import '@/styles/tailwind.css';
 
 const plusJakartaSans = Plus_Jakarta_Sans({
   subsets: ['latin'],
